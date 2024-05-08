cd215d6cba37fe50ce04837c46fed1c235a02167
Avinash01 <avinash.m1302@gmail.com>
2024-05-08 22:15:07 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+import Card from "./Components/Card"
+
+function App() {
+
+  return (
+    <>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <Card></Card>
+    </>
+  )
+}
+
+export default App
